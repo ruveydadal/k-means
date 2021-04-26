531754bc379893ab7c2f2a5257c3844ef6635432
Rüveyda Dal <71987230+ruv19@users.noreply.github.com>
2021-04-26 17:20:13 +0300

    Add files via upload

---
+++ k_ortalama.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 11 23:16:20 2021
+
+@author: muham
+"""
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+veriler=pd.read_csv('musteriler.csv')
+
+X=veriler.iloc[:,3:].values
+
+#KMEANS 
+from sklearn.cluster import KMeans
+kmeans=KMeans(n_clusters=3,init='k-means++')
+kmeans.fit(X)
+
+print(kmeans.cluster_centers_)
+sonuclar=[]
+for i in range(1,11):
+    kmeans=KMeans(n_clusters=i,init='k-means++',random_state=123)
+    kmeans.fit(X)
+    sonuclar.append(kmeans.inertia_)
+
+plt.plot(range(1,11),sonuclar)
+plt.show()
+
+#HİERARCHİCAL CLUSTER
+from sklearn.cluster import AgglomerativeClustering
+ac=AgglomerativeClustering(n_clusters=3,affinity='euclidean',linkage='ward')
+Y_tahmin=ac.fit_predict(X)
+print(Y_tahmin)
+
+plt.scatter(X[Y_tahmin==0,0],X[Y_tahmin==0,1],s=100,c='red')
+plt.scatter(X[Y_tahmin==1,0],X[Y_tahmin==1,1],s=100,c='blue')
+plt.scatter(X[Y_tahmin==2,0],X[Y_tahmin==2,1],s=100,c='green')
+plt.show()
+
+#dendrogram oluşturma
+import scipy.cluster.hierarchy as sch
+dendrogram=sch.dendrogram(sch.linkage(X,method='ward'))
+plt.show()
